c2a3e20d77182aaa96f8a2584fadb264ee7818de
vutrian576 <vutrian576@gmail.com>
2022-04-09 11:53:58 +0700

    ok

--- google_colab.py
+++ google_colab.py
@@ -47,12 +47,12 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',  # This is another valid field
 }
 def ColabSimple():
-      time.sleep(random.randint(3,8))
+      time.sleep(random.randint(10,20))
       if cl.count_documents({})>0:
         try:
           # h=h+1
           keyword = cl.find_one_and_delete({})
-          print(keyword)
+          print(keyword["keyword"]["Keyword"])
           if keyword:
                 if keyword["campaign"]["language"] == "vi":
                   try:
